0802caeaf65fe7a787f0fdb303180ffc342c67e7
Maicol <72515186+maicol240@users.noreply.github.com>
2024-09-24 22:33:55 -0400

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+    ],
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/index.html'],
+  },
+});
