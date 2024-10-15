ac5b3c0ca17e1056b1e67856fc887fceebe09843
Maicol <72515186+maicol240@users.noreply.github.com>
2024-10-15 13:03:49 -0400

    feat: Better display for game status     #Shows Current Players turn     #Shows who won in the page instead of an alert

--- src/ui-manager/ui-manager.js
+++ src/ui-manager/ui-manager.js
@@ -1,11 +1,18 @@
 import { GameManager } from '../game/game-manager';
 
 //will create my ui
-function createUI(board) {
-  let main = document.querySelector('main');
-  let boardContainer = document.createElement('div');
+function createUI(board, player) {
+  const main = document.querySelector('main');
 
+  const gameStatus = document.createElement('h2');
+  gameStatus.textContent = player + "'s Turn";
+  gameStatus.classList.add('game-status');
+
+  const boardContainer = document.createElement('div');
   boardContainer.classList = 'board-container';
+
+  main.appendChild(gameStatus);
+
   main.appendChild(boardContainer);
 
   for (let i = 0; i < 9; i++) {
@@ -23,38 +30,45 @@ function createUI(board) {
 }
 
 //render new game
-function restartGame(buttons, gameManager) {
+function restartGame(buttons, gameManager, gameStatus) {
   buttons.forEach((button) => {
     button.disabled = false;
     button.textContent = '';
 
     // Set the disabled property to true
   });
-
   gameManager.startNewGame();
+  gameStatus.textContent = gameManager.currentPlayer.name + "'s Turn";
 }
 
-/// WILL ADD EVENT LISTENER TO BTNS AND place the mark if valid
 export function startGame(gameManager) {
+  // render player names if user enter names
   renderNames(gameManager.player1, gameManager.player2);
-  createUI(gameManager.gameBoard);
+  createUI(gameManager.gameBoard, gameManager.currentPlayer.name);
+
+  const gameStatus = document.querySelector('.game-status');
   const buttons = document.querySelectorAll('.cell');
   const resetButton = document.querySelector('.reset');
+
   resetButton.addEventListener('click', () => {
-    restartGame(buttons, gameManager);
+    restartGame(buttons, gameManager, gameStatus);
   });
 
   buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
       ///display will display content if the return is valid
-      //will check for boolean or string
       const displayController = gameManager.play(index);
 
+      //if displayController is boolean it will disable the button  and update the board cell with the current player's marker
       if (displayController) {
         button.textContent = gameManager.gameBoard.cell[index];
-        button.disabled = true; //prevent playing in the same cell twice
+
+        //prevent playing in the same cell twice
+        button.disabled = true;
+        gameStatus.textContent = gameManager.currentPlayer.name + "'s Turn";
       }
 
+      // if displayController is string it will display the result after winning or getting the draw
       if (typeof displayController === 'string') {
         buttons.disabled;
         button.textContent = gameManager.gameBoard.cell[index];
@@ -63,8 +77,8 @@ export function startGame(gameManager) {
           button.disabled = true;
         });
         setTimeout(() => {
-          alert(displayController);
-        }, 500);
+          gameStatus.textContent = displayController;
+        }, 100);
       }
     });
   });
